e01e25999d86ca7f793778084d9652914258bcd6
OpenShift guest <rogueconformist@gmail.com>
2013-05-04 22:03:23 -0400

    adding in time slice

--- scripts/app/site.js
+++ scripts/app/site.js
@@ -1,5 +1,6 @@
 function SiteCtrl($scope, $routeParams, $location) {
   $scope.unityProjects = ["Atomic Pinball","Psychosurgeon","r/Knights of New","Shell",];
+  $scope.webProjects = ["Time Slice","Ico-Ico",];
     $scope.site = {tab: ""};
     $scope.gotoUnity = function(index){
       switch(index){
@@ -8,6 +9,16 @@ function SiteCtrl($scope, $routeParams, $location) {
               break;
           case 1:
               $location.url("/psycho");
+              break;
+      }  
+    };
+    $scope.gotoWeb = function(index){
+      switch(index){
+          case 0:
+              $location.url("/time");
+              break;
+          case 1:
+              $location.url("/icoico");
               break;
       }  
     };
@@ -31,6 +42,8 @@ angular.module('site', []).
       when('/psycho', {templateUrl: 'partials/psycho.html'}).
       when('/resume', {templateUrl: 'partials/resume.html'}).
       when('/python', {templateUrl: 'partials/python.html'}).
+      when('/time', {templateUrl: 'partials/time.html'}).
+      when('/icoico', {templateUrl: 'partials/ico.html'}).
       otherwise({redirectTo: '/'});
 }]);
   
