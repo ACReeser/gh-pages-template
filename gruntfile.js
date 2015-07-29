b35fc32c4687c3aa26733c1ab5970436b2a28575
ACReeser <rogueconformist@gmail.com>
2015-07-28 19:39:33 -0500

    Adding in grunt and zetzer

---
+++ gruntfile.js
@@ -0,0 +1,27 @@
+﻿module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        zetzer: {
+            main: {
+                //options: {
+                //    partials: "content",
+                //    templates: "templates"
+                //},
+                files: [
+                  {
+                      expand: true,
+                      //cwd: "",
+                      src: "*.partial.html",
+                      dest: "/",
+                      ext: ".html",
+                      flatten: true
+                  }
+                ]
+            },
+        }
+    });
+    grunt.loadNpmTasks('grunt-zetzer');
+
+    grunt.registerTask('default', ['zetzer']);
+};
